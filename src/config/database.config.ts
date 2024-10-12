48d31cbd9473d0589b16298a0972a5d40e2eae76
kishandsyncortex <kishandholakiya1027@gmail.com>
2024-10-12 14:12:22 +0530

    changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,3 +1,92 @@
+// import { DataSource } from "typeorm";
+// import "reflect-metadata";
+// import { User } from "../entities/user.entity";
+// import { AboutUsService, TokenService, UserService } from "../utils/admin";
+// import { Banner } from "../entities/banner.entity";
+// import { VisionExperties } from "../entities/vision_experties.entity";
+// import { CoreServices } from "../entities/core_services.entity";
+// import { SubServices } from "../entities/sub_services.entity";
+// import { TechnologicalExperties } from "../entities/technological_experties.entity";
+// import { Portfolio } from "../entities/portfolio.entity";
+// import { TrustedClients } from "../entities/trusted_clients.entity";
+// import { Blog } from "../entities/blog.entity";
+// import { QuestionAns } from "../entities/question_ans.entity";
+// import { AboutUs } from "../entities/about_us.entity";
+// import { CertificationDetails } from "../entities/certification_details.entity";
+// import { HowWeWork } from "../entities/how_we_work.entity";
+// import { EmployeeDetails } from "../entities/employee_details.entity";
+// import { ProvidedService } from "../entities/provided_service.entity";
+// import { FeaturedServices } from "../entities/featured_services.entity";
+// import { OurContactDetails } from "../entities/our_contact_details.entity";
+// import { Marketing } from "../entities/marketing.entity";
+// import { ContactUs } from "../entities/contact_us.entity";
+// import { PolicyAndTerms } from "../entities/policy_and_terms.entity";
+// import { ChatBoat } from "../entities/chat_boat.entity";
+// import { Token } from "../entities/token.entity";
+// import { DataCleanupScheduler } from "../utils/crone";
+// import logger from "../utils/winston";
+// import DB from "../config/variables/database.json";
+
+// const databaseConfigurations = DB;
+
+// const host = databaseConfigurations.HOST;
+// const database_port = parseInt(databaseConfigurations.DATABASE_PORT);
+// const user_name = databaseConfigurations.USER_NAME;
+// const password = databaseConfigurations.PASSWORD;
+// const database = databaseConfigurations.DATABASE;
+
+// export const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: host,
+//   port: database_port,
+//   username: user_name,
+//   password: password,
+//   database: database,
+//   synchronize: false,
+//   logging: false,
+//   entities: [
+//     Banner,
+//     User,
+//     VisionExperties,
+//     CoreServices,
+//     SubServices,
+//     TechnologicalExperties,
+//     Portfolio,
+//     TrustedClients,
+//     Blog,
+//     QuestionAns,
+//     AboutUs,
+//     CertificationDetails,
+//     HowWeWork,
+//     EmployeeDetails,
+//     ProvidedService,
+//     FeaturedServices,
+//     OurContactDetails,
+//     Marketing,
+//     ContactUs,
+//     PolicyAndTerms,
+//     ChatBoat,
+//     Token,
+//   ],
+// });
+
+// (async () => {
+//   try {
+//     await AppDataSource.initialize();
+//     logger.info("Database connected successfully!");
+//     const userService = new UserService(AppDataSource);
+//     await userService.createAdmin();
+//     const addAboutUs = new AboutUsService(AppDataSource);
+//     addAboutUs.createAboutsUsPage();
+//     const addToken = new TokenService(AppDataSource);
+//     addToken.createToken();
+//     const dataCleanupScheduler = new DataCleanupScheduler();
+//     dataCleanupScheduler.start();
+//   } catch (error) {
+//     logger.error("Error during Data Source initialization", error);
+//   }
+// })();
+
 import { DataSource } from "typeorm";
 import "reflect-metadata";
 import { User } from "../entities/user.entity";
@@ -26,6 +115,8 @@ import { Token } from "../entities/token.entity";
 import { DataCleanupScheduler } from "../utils/crone";
 import logger from "../utils/winston";
 import DB from "../config/variables/database.json";
+import path from "path";
+import fs from "fs";
 
 const databaseConfigurations = DB;
 
@@ -34,6 +125,7 @@ const database_port = parseInt(databaseConfigurations.DATABASE_PORT);
 const user_name = databaseConfigurations.USER_NAME;
 const password = databaseConfigurations.PASSWORD;
 const database = databaseConfigurations.DATABASE;
+const caPath = path.join(__dirname, "./keys/ca.pem");
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -42,8 +134,12 @@ export const AppDataSource = new DataSource({
   username: user_name,
   password: password,
   database: database,
-  synchronize: false,
+  synchronize: true,
   logging: false,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(caPath),
+  },
   entities: [
     Banner,
     User,
@@ -83,6 +179,15 @@ export const AppDataSource = new DataSource({
     const dataCleanupScheduler = new DataCleanupScheduler();
     dataCleanupScheduler.start();
   } catch (error) {
+    console.error(error);
     logger.error("Error during Data Source initialization", error);
   }
 })();
+
+// {
+//   "HOST": "aws-0-ap-south-1.pooler.supabase.com",
+//   "DATABASE_PORT": "6543",
+//   "USER_NAME": "postgres.vtzwwasnuycnxpgrpfuz",
+//   "PASSWORD": "Abc@123.com",
+//   "DATABASE": "postgres"
+// }
